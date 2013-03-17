dccf0325bba43c6094c1b5d8324a244c945ddb87
shapr <shae@ScannedInAvian.com>
2013-03-17 17:27:45 -0500

    main logic written, but not tested

--- eight.py
+++ eight.py
@@ -3,31 +3,28 @@
 # the empty tile is the zero position, 
 
 
-def show_state(state):
+def show_board(board):
     line = "-------"
-    top = "|%s|%s|%s|" % tuple([tile_at(state,pos) for pos in [(1,1),(2,1),(3,1)]])
-    mid = "|%s|%s|%s|" % tuple([tile_at(state,pos) for pos in [(1,2),(2,2),(3,2)]])
-    bot = "|%s|%s|%s|" % tuple([tile_at(state,pos) for pos in [(1,3),(2,3),(3,3)]])
+    top = "|%s|%s|%s|" % tuple([tile_at(board,pos) for pos in [(1,1),(2,1),(3,1)]])
+    mid = "|%s|%s|%s|" % tuple([tile_at(board,pos) for pos in [(1,2),(2,2),(3,2)]])
+    bot = "|%s|%s|%s|" % tuple([tile_at(board,pos) for pos in [(1,3),(2,3),(3,3)]])
     return '\n'.join([line,top,line,mid,line,bot,line])
 
-## ((2, 2), (1, 1), (2, 1), (3, 1), (1, 2), (3, 2), (1, 3), (2, 3), (3, 3)) == create_state("123405678")
+## ((2, 2), (1, 1), (2, 1), (3, 1), (1, 2), (3, 2), (1, 3), (2, 3), (3, 3)) == create_board("123405678")
 #. True
 
-def create_state(numbers):
+def create_board(numbers):
     # must have at least nine numbers in the input string, will act weird if they're not unique
     # 123405678
     positions = [(x,y) for y in [1,2,3] for x in [1,2,3]] # order is important here!
-    state = zip(numbers,positions)
-    state.sort() # now ordered correctly
-    return tuple(map(lambda x:x[1],state)) # strip the values, leave only positions
+    board = zip(numbers,positions)
+    board.sort() # now ordered correctly
+    return tuple(map(lambda x:x[1],board)) # strip the values, leave only positions
 
-## 0 == tile_at(create_state("123405678"),(2,2))
+## 0 == tile_at(create_board("123405678"),(2,2))
 #. True
 def tile_at(state,(x,y)):
     return state.index((x,y))
-
-## 2 + 3
-#. 5
 
 nil = []
 ## 0 == dist((1,1),(1,1))
@@ -37,7 +34,7 @@ nil = []
 def dist(a,b):
     return abs(a[0]-b[0]) + abs(a[1]-b[1])
 
-## 1 == state_dist(create_state("123405678"),create_state("123045678"))
+## 1 == state_dist(create_board("123405678"),create_board("123045678"))
 #. True
 def state_dist(sa,sb):
     # must leave out the zero for this to work, [1:] drops the first (zero) item
@@ -57,7 +54,7 @@ def legal_moves(state):
     xmoves = [(a,y) for a in moves[x]]
     return ymoves + xmoves
 
-## apply_move(create_state("123405678"),(1,2)) == create_state("123045678")
+## apply_move(create_board("123405678"),(1,2)) == create_board("123045678")
 #. True
 def apply_move(state,move):
     tileindex  = tile_at(state,move) # where's the tile to swap with zero?
@@ -68,21 +65,74 @@ def apply_move(state,move):
     state[tileindex] = zero # put the previous zero tile location into the other tile location
     return tuple(state)
 
-## len(succ_states(create_state("123405678"))) == 4
+## len(succ_states(create_board("123405678"))) == 4
 #. True
 def succ_states(state):
     # which tiles could zero tile move to from here?
     return [apply_move(state,m) for m in legal_moves(state)]
 
-# pair = (x,y)
-#blank = [(0,0) for n in xrange(1,10)]
-# 123
-# 405
-# 678
-start = create_state("123405678")
-# or [(2,2),(1,1),(2,1),(3,1),(1,2),(3,2),(1,3),(2,3),(3,3)]
+def goal_found(s,node):
+    print "goal has been found"
+    print "steps are:"
+    steps = []
+    while(node[3]):
+        steps.append(node[2])
+        node = node[3]
+    steps.reverse()
+    for s in steps:
+        print showstate(s)
 
-oneaway = [(2,2),(1,1),(2,1),(3,1),(3,2),(3,3),(2,3),(1,3),(1,2)]
+start_board = create_board("617285340")
+goal_board = create_board("187206345")
+
+## state_dist(start_board,goal_board) == 6
+#. True
+
+# priority will always be (moves_from_start + heuristic_to_goal)
+# second value is moves from start, for easy incrementing
+# third value is the board_state
+# fourth value is the parent board_state, None for the start_board
+# this returns the open set as a priority queue
+def init_state(start_board,goal_board):
+    return [(state_dist(start_board_state,goal_board),0,start_board,None)]
+# closed set, dictionary for easy lookup
+closed = {}
+# open set is the priority queue from init_state
+def solve_step(pqueue,closed):
+    if(not pqueue):
+        # no more states to explore, no solution found
+        # do something useful here!
+        pass
+    pqueue.sort() # sort in order of moves_from_start + heuristic_to_goal
+    parent_node = pqueue.pop() # get the least cost node
+    # unpack the four tuple, ignore the sort value
+    p,parent_moves,parent_board_state,parent = parent_node
+
+    closed[parent_board_state] = parent_moves
+
+    # find the successor board states
+    # if they're in the closed set, don't add them XXX should this check closed set cost and update?
+    # otherwise, calculate their heuristic cost, add that to the parent move value + 1 and return the new state
+    succs = succ_states(parent_board_state)
+    for s in succs:
+        this_moves = parent_moves + 1
+        this_dist  = state_dist(s,goal)
+        if (this_dist == 0):
+            # goal FOUND, we are DONE!
+            goal_found(s,parent_node)
+            return '' # umm, termination condition?
+        if closed.has_key(s):
+            if(closed[s] > this_moves): # aha! a shorter path to this node! UPDATE THE PQUEUE!
+                pqueue.append(this_moves + this_dist,this_moves,s,parent_node)
+            continue # We've already got one, NEXT!
+        pqueue.append(this_moves + this_dist,this_moves,s,parent_node)
+    return (pqueue,closed)
+
+def __main__():
+    init = init_state(create_board("123405678"),create_board("123405678"))
+    closed = {}
+    while (solve_step(init,closed)):
+        print 'next step!'
 
 # board state also keeps g (cost so far)
 # possibly h, possibly g, possibly f
